e233d1f3f6e4ecda1db4a263f50f7869a1ae8046
Dan Hassin <dingbat@users.noreply.github.com>
2016-03-03 20:47:14 +0000

    final thing

--- site.js
+++ site.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
     $.getJSON("https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/master/weather.json", function( data ) {
-        console.log('reload');
-        console.log('reload!');
         var total = 0;
         for (var i = 0; i < data.length; i++)
         {
-            total += parseInt(data.TemperatureF);
+            total += parseInt(data[i].TemperatureF);
             $('#weather').append("<li>"+data[i].TimeEST+" "+data[i].TemperatureF+"</li>");
         }
         total /= data.length;
