19399af20cb3bac0efe998f363c8140d43e7fddb
Dan Hassin <dingbat@users.noreply.github.com>
2016-03-03 20:46:28 +0000

    shows average weather

--- site.js
+++ site.js
@@ -1,6 +1,15 @@
-console.log("Hello world");
-
 $(document).ready(function() {
-    console.log("rejoice - jquery is enabled!");
+    $.getJSON("https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/master/weather.json", function( data ) {
+        console.log('reload');
+        console.log('reload!');
+        var total = 0;
+        for (var i = 0; i < data.length; i++)
+        {
+            total += parseInt(data.TemperatureF);
+            $('#weather').append("<li>"+data[i].TimeEST+" "+data[i].TemperatureF+"</li>");
+        }
+        total /= data.length;
+        $('#average').html("Average "+total);
+    });
 });
 
